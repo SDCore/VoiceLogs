c13e6150a87a09fb4992d4a0fb0b12a297c10524
SDCore <sdcored@gmail.com>
2023-08-23 00:58:00 -0500

    Optimization Updates

--- VoiceLogs.js
+++ VoiceLogs.js
@@ -1,15 +1,21 @@
 const dotenv = require('dotenv');
 const { Client, GatewayIntentBits } = require('discord.js');
+const { Guilds, GuildVoiceStates } = GatewayIntentBits;
 
 dotenv.config();
 
+const { loadEvents } = require('./loadEvents.js');
+
 const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+	intents: [Guilds, GuildVoiceStates],
 	allowedMentions: { users: [] },
 });
 
+client
+	.login(process.env.DISCORD_TOKEN)
+	.then(() => {
+		loadEvents(client);
+	})
+	.catch(err => console.log(err));
+
 module.exports = { client };
-
-require('./handler.js')(client);
-
-client.login(process.env.DISCORD_TOKEN);
--- events/client/VoiceStateUpdate.js
+++ events/client/VoiceStateUpdate.js
@@ -1,30 +1,30 @@
-const { client } = require('../VoiceLogs.js');
+const { client } = require('../../VoiceLogs.js');
 const { DateTime } = require('luxon');
-const log = require('../logList.json');
+const log = require('../../logList.json');
 const chalk = require('chalk');
-const emotes = require('../emotes.json');
+const emotes = require('../../emotes.json');
 
 client.on('voiceStateUpdate', (oldState, newState) => {
 	const time = Math.floor(DateTime.local().toMillis() / 1000);
 
 	if (oldState.channel != null) {
 		if (log[oldState.channel.parent.id] == null || log[oldState.channel.parent.id] == 'undefined')
-			return console.log(chalk.red(`${chalk.bold('[VOICE]')} Could not log voice state update because its category is not in the log list.`));
+			return console.log(chalk.red(`${chalk.bold('[VOICE]')} Could not log voice state update because its category is not in the log list`));
 	}
 
 	if (newState.channel != null) {
 		if (log[newState.channel.parent.id] == null || log[newState.channel.parent.id] == 'undefined')
-			return console.log(chalk.red(`${chalk.bold('[VOICE]')} Could not log voice state update because its category is not in the log list.`));
+			return console.log(chalk.red(`${chalk.bold('[VOICE]')} Could not log voice state update because its category is not in the log list`));
 	}
 
 	if (oldState.channelId === null) {
 		// User Joins VC
-		console.log(chalk.green(`${chalk.bold('[VOICE]')} User ${newState.member.user.username}#${newState.member.user.discriminator} Joined #${newState.channel.name}.`));
+		console.log(chalk.green(`${chalk.bold('[VOICE]')} User ${newState.member.user.username}#${newState.member.user.discriminator} Joined #${newState.channel.name}`));
 
 		var voice = `<t:${time}:f> :microphone2: ${emotes.Join} <@${newState.member.id}> (**${newState.member.user.username}#${newState.member.user.discriminator}**, \`${newState.member.id}\`) joined <#${newState.channel.id}> (**#${newState.channel.name}**, \`${newState.channel.id}\`)`;
 	} else if (newState.channelId === null) {
 		// User Leaves VC
-		console.log(chalk.red(`${chalk.bold('[VOICE]')} User ${oldState.member.user.username}#${oldState.member.user.discriminator} Left #${oldState.channel.name}.`));
+		console.log(chalk.red(`${chalk.bold('[VOICE]')} User ${oldState.member.user.username}#${oldState.member.user.discriminator} Left #${oldState.channel.name}`));
 
 		var voice = `<t:${time}:f> :microphone2: ${emotes.Leave} <@${oldState.member.id}> (**${oldState.member.user.username}#${oldState.member.user.discriminator}**, \`${oldState.member.id}\`) left <#${oldState.channel.id}> (**#${oldState.channel.name}**, \`${oldState.channel.id}\`)`;
 	} else if (oldState.channelId != newState.channelId) {
@@ -33,7 +33,7 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 			chalk.yellow(
 				`${chalk.bold('[VOICE]')} User ${oldState.member.user.username}#${oldState.member.user.discriminator} Moved From #${oldState.channel.name} To #${
 					newState.channel.name
-				}.`,
+				}`,
 			),
 		);
 
